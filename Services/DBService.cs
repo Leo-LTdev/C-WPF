83a0fa9da383f149a523f200133613f63fd365bd
Leo-byno <leo.toscano@ynov.com>
2026-04-25 01:32:06 +0200

    hotfix: add spells

--- Services/DBService.cs
+++ Services/DBService.cs
@@ -21,12 +21,23 @@ namespace WpfApp1.Services
                 PasswordHash = _authService.HashEnBase("admin123")
             };
             _context.Logins.Add(admin);
-            var warrior = new Hero { Name = "Guerrier", Health = 150 };
-            var mage = new Hero { Name = "Mage", Health = 90 };
-            _context.Heroes.AddRange(warrior, mage);
             var slash = new Spell { Name = "Coup d'épée", Damage = 25, Description = "Attaque de base" };
             var fireball = new Spell { Name = "Boule de feu", Damage = 40, Description = "Attaque magique" };
-            _context.Spells.AddRange(slash, fireball);
+            var heal = new Spell { Name = "Soin", Damage = -20, Description = "Rend des points de vie" };
+            _context.Spells.AddRange(slash, fireball, heal);
+            var warrior = new Hero
+            {
+                Name = "Guerrier",
+                Health = 150,
+                Spells = new System.Collections.Generic.List<Spell> { slash }
+            };
+            var mage = new Hero
+            {
+                Name = "Mage",
+                Health = 90,
+                Spells = new System.Collections.Generic.List<Spell> { fireball, heal }
+            };
+            _context.Heroes.AddRange(warrior, mage);
             _context.SaveChanges(); // save DB
         }
     }
