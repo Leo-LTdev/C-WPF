891063f44617c23fca760348f16ba1b1e3d4492f
Leo-byno <leo.toscano@ynov.com>
2026-04-25 01:05:29 +0200

    Feat: Hero Management done

---
+++ Services/HeroService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WpfApp1.Models;
+
+namespace WpfApp1.Services
+{
+    public class HeroService
+    {
+        private readonly AppDbContext _context;
+        public HeroService()
+        {
+            _context = new AppDbContext();
+        }
+        public List<Hero> GetAllHeroes()
+        {
+            return _context.Heroes.Include(h => h.Spells).ToList();
+        }
+    }
+}
---
+++ ViewModels/HeroManagement.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using WpfApp1.Models;
+using WpfApp1.Services;
+
+namespace WpfApp1.ViewModels
+{
+    public partial class HeroManagement : ObservableObject
+    {
+        private readonly HeroService _heroService;
+        [ObservableProperty]
+        private ObservableCollection<Hero> _heroes;
+        [ObservableProperty]
+        private Hero? _selectedHero;
+        public HeroManagement()
+        {
+            _heroService = new HeroService();
+            Heroes = new ObservableCollection<Hero>();
+            LoadData();
+        }
+        [RelayCommand]
+        private void LoadData()
+        {
+            var data = _heroService.GetAllHeroes();
+            Heroes = new ObservableCollection<Hero>(data);
+        }
+    }
+}
